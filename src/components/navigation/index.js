a11e5ede4ab496be61cef82108c307f4a9bfcf6f
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-27 17:44:56 +0300

    initial design for navigation

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './navigation.css';
 
 const Navigation = () => {
-    // In the navigation panel, the user can see links to:
-
-    // Motorcycles/doctors/classes/items that you selected as a theme.
-    // "Reserve" form.
-    // "My reservations".
-    // "Add motorcycle/doctor/class/item that you selected as a theme" (the link is visible to everybody).
-    // "Delete motorcycle/doctor/class/item that you selected as a theme" (the link is visible to everybody).
-
-  return <div>Navigation</div>;
+  return (
+    <nav id='navigation'>
+      <div className="hamburger">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <h1>BookDoc</h1>
+      <ul>
+        <li>
+          <Link to='/doctors'>Doctors</Link>
+        </li>
+        <li>
+          <Link to='/reserve'>Reserve</Link>
+        </li>
+        <li>
+          <Link to='/reservations'>My reservations</Link>
+        </li>
+        <li>
+          <Link to='/doctors/new'>Add doctor</Link>
+        </li>
+        <li>
+          <Link to='/'>Delete doctor</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
