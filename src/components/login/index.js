de8a9d45e60b56922be6dd0614df1f0f4a153623
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-27 17:23:19 +0300

    fix linters

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './login.css';
 
-const LoginPage = () => {
-  return (
-    <div id='login-container'>
-      <div className='login-div'>
-        <h1>BookDoc</h1>
+const LoginPage = () => (
+  <div id="login-container">
+    <div className="login-div">
+      <h1>BookDoc</h1>
 
-        <form id='login-form'>
-          <input type='text' placeholder='Enter your username...' />
-          <button type='submit'>Login</button>
-        </form>
-      </div>
+      <form id="login-form">
+        <input type="text" placeholder="Enter your username..." />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  );
-};
+  </div>
+);
 
 export default LoginPage;
