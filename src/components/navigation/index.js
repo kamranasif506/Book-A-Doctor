f358d937a1bb60a8f13b1e1627ba63980a3d00d6
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-27 17:27:54 +0300

    add navigation component

---
+++ src/components/navigation/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navigation = () => {
+    // In the navigation panel, the user can see links to:
+
+    // Motorcycles/doctors/classes/items that you selected as a theme.
+    // "Reserve" form.
+    // "My reservations".
+    // "Add motorcycle/doctor/class/item that you selected as a theme" (the link is visible to everybody).
+    // "Delete motorcycle/doctor/class/item that you selected as a theme" (the link is visible to everybody).
+
+  return <div>Navigation</div>;
+};
+
+export default Navigation;
