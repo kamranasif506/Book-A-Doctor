6f256dc423994ac36fbba41709ef24523754ee3b
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-28 18:03:50 +0300

    add side navbar

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TbX } from 'react-icons/tb';
 import './navigation.css';
 
 const Navigation = () => (
-  <nav id="navigation">
-    <div className="hamburger">
-      <div className="line" />
-      <div className="line" />
-      <div className="line" />
+  <nav id='navigation'>
+    <div className='hamburger'>
+      <div className='line' />
+      <div className='line' />
+      <div className='line' />
     </div>
 
     <h1>BookDoc</h1>
 
     <ul>
+      <TbX className='icon' />
       <li>
-        <Link to="/doctors">Doctors</Link>
+        <Link to='/doctors'>Doctors</Link>
       </li>
       <li>
-        <Link to="/reserve">Reserve</Link>
+        <Link to='/reserve'>Reserve</Link>
       </li>
       <li>
-        <Link to="/reservations">My reservations</Link>
+        <Link to='/reservations'>My reservations</Link>
       </li>
       <li>
-        <Link to="/doctors/new">Add doctor</Link>
+        <Link to='/doctors/new'>Add doctor</Link>
       </li>
       <li>
-        <Link to="/">Delete doctor</Link>
+        <Link to='/'>Delete doctor</Link>
       </li>
     </ul>
   </nav>
