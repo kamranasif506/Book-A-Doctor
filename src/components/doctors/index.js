03fbf45677ea1bd329ccd76fedae0ce9fc869e5b
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-28 12:26:03 +0300

    add scroll buttons fpr doctors

--- src/components/doctors/index.js
+++ src/components/doctors/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import photo from '../../hack-4.jpg';
 import Doctor from './Doctor';
 import './doctors.css';
 
 const Doctors = () => {
+  const docsRef = useRef()
+  const docScrollHandler = (direction) => {
+    if (!docsRef) return;
+
+    direction === 'left'
+      ? (docsRef.current.scrollLeft -= 300)
+      : (docsRef.current.scrollLeft += 300);
+  };
+
   const testDocs = [
     {
       id: 1,
@@ -23,7 +32,15 @@ const Doctors = () => {
     },
     {
       id: 3,
-      name: 'doc2',
+      name: 'doc3',
+      photo,
+      desc: 'Some description about the doctor. This will be coming from the backend doctors database.',
+      fbLink: '',
+      twitterLink: '',
+    },
+    {
+      id: 4,
+      name: 'doc4',
       photo,
       desc: 'Some description about the doctor. This will be coming from the backend doctors database.',
       fbLink: '',
@@ -38,11 +55,19 @@ const Doctors = () => {
         <div id='dots'></div>
       </div>
 
-      <div id='doctors'>
+      <div id='doctors' ref={docsRef}>
         {testDocs.map((doc) => (
           <Doctor doctor={doc} key={doc.id} />
         ))}
       </div>
+      <div
+        id='scroll-left'
+        onClick={() => docScrollHandler('left')}
+      ></div>
+      <div
+        id='scroll-right'
+        onClick={() => docScrollHandler('right')}
+      ></div>
     </div>
   );
 };
