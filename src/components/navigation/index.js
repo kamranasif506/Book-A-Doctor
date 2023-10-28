9319acf53be1d501a9fd5d3aca9cd9720e5e7f07
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-28 20:32:02 +0300

    add doctorDetail media queries

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -5,13 +5,48 @@ import { useDispatch, useSelector } from 'react-redux';
 import { navActions } from '../../redux/navbar/navSlice';
 import './navigation.css';
 
+const links = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'Doctors',
+    path: '/doctors',
+  },
+  {
+    name: 'Reserve',
+    path: '/reserve',
+  },
+  {
+    name: 'My reservations',
+    path: '/reservations',
+  },
+  {
+    name: 'Add doctor',
+    path: '/doctors/new',
+  },
+  {
+    name: 'Delete doctor',
+    path: '/',
+  },
+];
+
 const Navigation = () => {
   const dispatch = useDispatch();
   const { isOpen, active } = useSelector((state) => state.navbar);
 
+  const linkClickHandler = (name) => {
+    dispatch(navActions.setActive(name));
+    dispatch(navActions.toggle());
+  };
+
   return (
     <nav id='navigation'>
-      <div className='hamburger' onClick={() => dispatch(navActions.toggle())}>
+      <div
+        className='hamburger'
+        onClick={() => dispatch(navActions.toggle())}
+      >
         <div className='line' />
         <div className='line' />
         <div className='line' />
@@ -20,22 +55,19 @@ const Navigation = () => {
       <h1>BookDoc</h1>
 
       <ul className={isOpen ? 'open' : ''}>
-        <TbX className='icon' onClick={() => dispatch(navActions.toggle())} />
-        <li>
-          <Link to='/doctors'>Doctors</Link>
-        </li>
-        <li>
-          <Link to='/reserve'>Reserve</Link>
-        </li>
-        <li>
-          <Link to='/reservations'>My reservations</Link>
-        </li>
-        <li>
-          <Link to='/doctors/new'>Add doctor</Link>
-        </li>
-        <li>
-          <Link to='/'>Delete doctor</Link>
-        </li>
+        <TbX
+          className='icon'
+          onClick={() => dispatch(navActions.toggle())}
+        />
+        {links.map((link) => (
+          <li
+            key={link.name}
+            className={link.name === active ? 'active' : ''}
+            onClick={() => linkClickHandler(link.name)}
+          >
+            <Link to={link.path}>{link.name}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
