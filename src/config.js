8278aa32472ff07d2c233c542ab3acb7e38bb8ee
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-11-02 15:00:10 +0500

    Setup config file to store api urls

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+    apiBaseUrl: 'http://127.0.0.1:4000/',
+    loginEndpoint: 'login',
+    signupEndpoint: 'signup',
+    logoutEndpoint: 'logout',
+    // Add more URLs as needed
+  };
+  
+  export default config;
