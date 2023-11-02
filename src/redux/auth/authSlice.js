0b3d6b5a5199c6c513e56a705b39d26f54c1cb88
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-11-02 15:17:47 +0500

    Setup reducer for authSlice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios'
+import config from '../../config';
 
 const initialState = {
     user: null,
@@ -7,18 +9,42 @@ const initialState = {
     error: null
 }
 
-const loginURl = 'http://127.0.0.1:4000/login'
+const loginURl = `${config.apiBaseUrl}${config.loginEndpoint}`;
 export const LoginAuth = createAsyncThunk(
     'auth/login',
-    async (credentials) => {
+    async (data, thunkAPI) => {
         try {
-            const response = await fetch(loginURl)
+            const response = await axios.post(loginURl,data)
+            console.log(response);
             const user = response.data.user; 
             const token = response.headers['authorization']; 
             localStorage.setItem('token', token);
 
             return { user, token };
         } catch (error) {
-            throw error;
+            throw thunkAPI.rejectWithValue(error);
         }
 })
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(LoginAuth.pending, (state,action) => {
+                state.status = 'pending'
+            })
+            .addCase(LoginAuth.fulfilled, (state, action) => {
+                state.user = action.payload.data
+                state.token = action.payload.token
+                state.status = action.payload.status.message
+            })
+            .addCase(LoginAuth.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+    }
+})
+
+export default authSlice.reducer;
