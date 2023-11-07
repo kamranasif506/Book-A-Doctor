2be3b409ec67fd89817d537bffd0999301ffbeac
akezeth <akezeth@gmail.com>
2023-11-07 13:20:14 +0300

    Update doctorSlice

--- src/redux/doctors/doctorSlice.js
+++ src/redux/doctors/doctorSlice.js
@@ -1,29 +1,24 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-
 // const dataUrl = 'http://localhost:4000/api/v1/doctors/:doctor_id/appointments';
 const token = localStorage.getItem('token');
-
 const urlappoint = 'http://localhost:4000/api/v1/doctors';
 const headers = {
   Authorization: localStorage.getItem('token'),
 };
-
 // Create an async thunk to fetch appointments
 export const getDoctors = createAsyncThunk('appointments/fetch', async () => {
   const response = await axios.get(urlappoint, { headers });
   return response.data;
 });
-
-export const postData = createAsyncThunk('reservation/postData', async (appointment) => axios.post(`http://localhost:4000/api/v1/doctors/${appointment.docId}/appointments`, appointment, {
+export const postData = createAsyncThunk('doctor/postData', async (doctor) => axios.post('http://localhost:4000/api/v1/doctors', doctor, {
   headers: {
     'Content-type': 'application/json',
     // eslint-disable-next-line quote-props
     'Authorization': token,
   },
-  body: JSON.stringify(appointment),
+  body: JSON.stringify(doctor),
 }).then((response) => response.data).catch((err) => console.log(err)));
-
 export const deleteData = createAsyncThunk('appointment/deleteData', async (itemid) => axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/McvW73kTCQbVPzcD7S2A/books/${itemid}`, {
   body: JSON.stringify({
     item_id: itemid,
@@ -33,12 +28,10 @@ export const deleteData = createAsyncThunk('appointment/deleteData', async (item
     'Content-type': 'application/json',
   },
 }).then((response) => response).catch((err) => console.log(err)));
-
 const initialState = {
   doctor: [],
   isLoading: true,
 };
-
 const doctorSlice = createSlice({
   name: 'doctor',
   initialState,
