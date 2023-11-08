bee9da14478032eb157544c16aa4fbe5827c6b08
hassanShakur <hass.shakurhassan@gmail.com>
2023-11-08 14:16:08 +0300

    update add doc to make all values required

--- src/components/addDoctor/AddDoctor.js
+++ src/components/addDoctor/AddDoctor.js
@@ -3,17 +3,37 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import './addDoctor.css';
 import { postData } from '../../redux/doctors/doctorSlice';
+import { useEffect } from 'react';
 
 export default function AddDoctor() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [city, setLocation] = useState('Select Location');
-  const [doctorName, setName] = useState('Name');
-  const [specializationId, setSpecialization] = useState('Specialization');
-  const [profilePicture, setPicture] = useState('Image');
-  const [docBio, setBio] = useState('Doctor bio');
-  const [pending, setPending] = useState('Add New Doctor');
-  const { specialization } = useSelector((store) => store.specialization);
+  const { specialization } = useSelector(
+    (store) => store.specialization
+  );
+
+  const [doctorName, setName] = useState('');
+  const [specializationId, setSpecialization] = useState(1);
+  const [city, setLocation] = useState('');
+  const [profilePicture, setPicture] = useState('');
+  const [docBio, setBio] = useState('');
+  const [pending, setPending] = useState('Add Doctor');
+  const [btnDisabled, setBtnDisabled] = useState(false);
+
+  useEffect(() => {
+    if (
+      doctorName === '' ||
+      specializationId === '' ||
+      city === '' ||
+      profilePicture === '' ||
+      docBio === ''
+    ) {
+      setBtnDisabled(true);
+    } else {
+      setBtnDisabled(false);
+    }
+  }, [doctorName, specializationId, city, profilePicture, docBio]);
+
   function postDispatcher() {
     const doctor = {
       location: city,
@@ -22,37 +42,46 @@ export default function AddDoctor() {
       profile_picture: profilePicture,
       bio: docBio,
     };
-    setPending('...Adding Doctor');
+    setPending('Adding Doctor...');
     setTimeout(() => {
       dispatch(postData(doctor));
-      setPending('Add Book');
+      setPending('Add Doctor');
       navigate('/doctors');
     }, 1000);
   }
   return (
-    <main className="add-doctor">
-      <div className="title-doc"><h1>ADD DOCTOR</h1></div>
-      <div className="doctor-container">
-        <form className="doctor-form">
-          <div className="doc-input-div">
-            <div className="doc-name-div">
-              <label htmlFor="name" className="doc-name-label">Name</label>
+    <main className='add-doctor'>
+      <div className='title-doc'>
+        <h1>ADD DOCTOR</h1>
+      </div>
+      <div className='doctor-container'>
+        <form className='doctor-form'>
+          <div className='doc-input-div'>
+            <div className='doc-name-div'>
+              <label htmlFor='name' className='doc-name-label'>
+                Name
+              </label>
               <input
-                className="name-input"
-                id="name"
-                placeholder="Name"
-                type="text"
-                name="doc_name"
+                className='name-input'
+                id='name'
+                placeholder='Name'
+                type='text'
+                name='doc_name'
+                required
                 onChange={(e) => {
                   setName(e.target.value);
                 }}
-                required
               />
             </div>
-            <div className="special-div">
-              <label htmlFor="specialization" className="special-label">Specialization</label>
+            <div className='special-div'>
+              <label
+                htmlFor='specialization'
+                className='special-label'
+              >
+                Specialization
+              </label>
               <select
-                className="special-input"
+                className='special-input'
                 value={specializationId}
                 onChange={(e) => {
                   setSpecialization(e.target.value);
@@ -60,45 +89,55 @@ export default function AddDoctor() {
                 required
               >
                 {specialization.map((option) => (
-                  <option key={option.value} value={option.id}>
+                  <option key={option.id} value={option.id}>
                     {option.name}
                   </option>
                 ))}
               </select>
             </div>
-            <div className="location-div">
-              <label htmlFor="location" className="doc-location-label">Location</label>
+            <div className='location-div'>
+              <label
+                htmlFor='location'
+                className='doc-location-label'
+              >
+                Location
+              </label>
               <input
-                className="location-input"
-                placeholder="Location"
-                type="text"
-                name="location"
+                className='location-input'
+                placeholder='Location'
+                type='text'
+                name='location'
+                required
                 onChange={(e) => {
                   setLocation(e.target.value);
                 }}
               />
             </div>
-            <div className="image-div">
-              <label htmlFor="image" className="photo-label">image</label>
+            <div className='image-div'>
+              <label htmlFor='image' className='photo-label'>
+                image
+              </label>
               <input
-                placeholder="Profile Picture URL"
-                className="image-input"
-                id="image"
-                type="text"
-                name="profile_pic"
+                placeholder='Profile Picture URL'
+                className='image-input'
+                id='image'
+                type='text'
+                name='profile_pic'
                 onChange={(e) => {
                   setPicture(e.target.value);
                 }}
-                required
               />
             </div>
-            <div className="bio-div">
-              <label htmlFor="bio" className="bio-label">Bio</label>
+            <div className='bio-div'>
+              <label htmlFor='bio' className='bio-label'>
+                Bio
+              </label>
               <textarea
-                className="bio-input"
-                id="bio"
-                placeholder="Bio"
-                name="bio"
+                className='bio-input'
+                id='bio'
+                placeholder='Bio'
+                name='bio'
+                required
                 onChange={(e) => {
                   setBio(e.target.value);
                 }}
@@ -106,8 +145,9 @@ export default function AddDoctor() {
             </div>
           </div>
           <button
-            type="submit"
-            className="doc-add-btn"
+            type='submit'
+            className='doc-add-btn'
+            disabled={btnDisabled}
             onClick={(e) => {
               e.preventDefault();
               postDispatcher();
