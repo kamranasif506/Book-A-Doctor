98b955da8cbb208b3084ba3a8d7a72969d637d87
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-11-09 17:22:52 +0500

    Fix Reserve doctor issue

--- src/redux/appointments/appointmentSlice.js
+++ src/redux/appointments/appointmentSlice.js
@@ -5,13 +5,11 @@ export const getAppointment = createAsyncThunk('reservation/getReservation', asy
   .then((res) => res.data)
   .catch((err) => thunkAPI.rejectWithValue(err)));
 
-const token = localStorage.getItem('token');
-
 export const postData = createAsyncThunk('reservation/postData', async (appointment, thunkAPI) => axios.post(`http://localhost:4000/api/v1/doctors/${appointment.docId}/appointments`, appointment, {
   headers: {
     'Content-type': 'application/json',
     // eslint-disable-next-line quote-props
-    'Authorization': token,
+    'Authorization': localStorage.getItem('token'),
   },
   body: JSON.stringify(appointment),
 }).then((response) => response.data).catch((err) => thunkAPI.rejectWithValue(err)));
